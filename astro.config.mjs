fa737b9a89ba8ff5fca21d240cebcec84b887745
arthursn1 <arthurninomiya@gmail.com>
2025-12-19 16:55:50 -0300

    Configura deploy github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://github.com/arthursn1', 
+  base: '/limpeza-sofa',
   vite: {
     plugins: [tailwindcss()],
   },
