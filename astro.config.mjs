2de35b7cd321eff1b998ec5ae4de67c7f78880db
arthursn1 <arthurninomiya@gmail.com>
2026-01-14 11:57:02 -0300

    correção do base do domínio

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://limpezasofa.online/', 
-  base: '/limpeza-sofa',
+  base: '/',
   vite: {
     plugins: [tailwindcss()],
   },
